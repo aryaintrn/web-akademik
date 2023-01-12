db4ed4c154c59fa3d8904e22267fa6404fbf53b7
INTRN <aryaintaran9@gmail.com>
2023-01-13 00:57:20 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Halaman</title>
+    </head>
+
+    <body>
+        <!-- LOGIN -->
+        <div class="page-login">
+            <div class="box">
+                <div class="box-header">
+                    LOGIN
+                </div>
+                <div class="box-body">
+                    <form>
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="user" placeholder="Username" class="input-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="pass" placeholder="Password" class="input-control>
+                        </div>
+                        <input type="submit" name="submit" value="value">
+                    </form>
+                </div>
+                <div classs="box-footer">
+                    <a href="index.php">Halaman Utama</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
