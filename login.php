b5ddea8f113561ad65ac13d9fb66c6d24da25577
INTRN <aryaintaran9@gmail.com>
2023-01-13 13:10:33 +0800

    Update login.php

--- login.php
+++ login.php
@@ -8,26 +8,48 @@
     <body>
         <!-- LOGIN -->
         <div class="page-login">
-            <div class="box">
-                <div class="box-header">
+            <div class="box box-login">
+
+                <div class="box-header text-center">
                     LOGIN
                 </div>
+
                 <div class="box-body">
-                    <form>
+                    <form action="" method="POST">
+
                         <div class="form-group">
                             <label>Username</label>
                             <input type="text" name="user" placeholder="Username" class="input-control">
                         </div>
+
                         <div class="form-group">
                             <label>Password</label>
                             <input type="password" name="pass" placeholder="Password" class="input-control">
                         </div>
-                        <input type="submit" name="Submit" value="Login">
+
+                        <input type="submit" name="Submit" value="Login" class="btn">
+
                     </form>
+
+                    <?php
+                        if(isset($_POST['Submit'])){
+                            $user = $_POST['user'];
+                            $pass = $_POST['pass'];
+
+                            if($user == "admin" && $pass == "admin"){
+                                echo "Login Berhasil";
+                            }else{
+                                echo "Login Gagal";
+                            }
+                        }
+                    ?>
+
                 </div>
-                <div classs="box-footer">
+
+                <div class="box-footer text-center">
                     <a href="index.php">Halaman Utama</a>
                 </div>
+
             </div>
         </div>
     </body>
